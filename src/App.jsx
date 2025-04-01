6f1087d0943e3005c323813c1640ad660b37d667
yutarohabu <hup0628@gmail.com>
2025-04-01 14:15:06 +0900

    エネルギーカウンター追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import './App.css'
 import { Cards } from './component/cards/cards'
+import { E_counter } from './component/e_counter/e_counter'
 
 function App() {
 
   return (
     <>
       <h1>Van-Kei</h1>
-      <div className="card">
-        <Cards />
+      <div className="contents">
+        <E_counter />
+        <div className="card">
+          <Cards />
+        </div>
       </div>
       {/* <p className="read-the-docs">
         数字をクリックするとリセットされます
---
+++ src/component/e_counter/e_counter.jsx
@@ -0,0 +1,17 @@
+import styles from "./e_counter.module.css"
+import { useState } from "react"
+
+export const E_counter = () => {
+    console.log('e_counter component throw')
+    const [ene, setEne] = useState(0)
+
+    return(
+        <div className={styles.contents_box}>
+            <button className={styles.up_button} onClick={()=>{if(ene < 10){setEne((count)=> count + 1)}}}>▲</button>
+            <div onClick={()=>setEne(0)}>
+                <p>{ene}</p>
+            </div>
+            <button className={styles.down_button} onClick={()=>{if(ene > 0){setEne((count)=> count - 1)}}}>▼</button>
+        </div>
+    )
+}
